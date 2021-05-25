1ec10ff0b7a215922774d726304b610b80f3f603
Alexey Mefodyev <al.mefodyev@yandex.ru>
2021-05-25 22:31:39 +0300

    Initial Commit

---
+++ LoginVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginVC
+//
+//  Created by Alexey Mefodyev on 25.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
